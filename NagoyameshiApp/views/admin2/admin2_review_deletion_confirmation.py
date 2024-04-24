28f62dab54742aaf969b091fdb11cb6cd10218e3
ayaconsa <aya.consadole@gmail.com>
2024-04-24 14:47:31 +0900

    admin2のレビュー投稿削除機能の修正

--- NagoyameshiApp/views/admin2/admin2_review_deletion_confirmation.py
+++ NagoyameshiApp/views/admin2/admin2_review_deletion_confirmation.py
@@ -7,8 +7,12 @@ from django.urls import reverse_lazy
 # ================== 管理者（サイト運営側）画面 ==================
 
 
-# 管理者（サイト運営者）トップページ
+# 投稿削除確認ページ
 class ReviewDeletionConfirmationView(DeleteView):
     model = Review
-    success_url = reverse_lazy('admin_review_confirmation')
     template_name = "NagoyameshiApp/admin2/admin_review_deletion_confirmation.html"
+
+    def get_success_url(self):
+        # 削除したレビューのpkを取得し、それを使用して成功時のURLを生成
+        restaurant_pk = self.object.restaurant.pk
+        return reverse_lazy('admin_review_confirmation', kwargs={'pk': restaurant_pk})
