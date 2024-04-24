4a8d1541b89ed4699a8319d0176a3ac45720bee9
ayaconsa <aya.consadole@gmail.com>
2024-04-25 06:41:55 +0900

    店舗一覧ページの理論実装（途中）

--- NagoyameshiApp/views/user/restaurant_list.py
+++ NagoyameshiApp/views/user/restaurant_list.py
@@ -1,13 +1,37 @@
 from django.views.generic import TemplateView
 from NagoyameshiApp.models.restaurant import Restaurant
+from django.core.paginator import Paginator
 
 # ================== ユーザー画面 ==================
 # ************** 非会員でも表示できる画面 **************
 
 # 店舗一覧
 class RestaurantListView(TemplateView):
-    model = Restaurant
     template_name = "NagoyameshiApp/user/restaurant_list.html"
-    # context_object_name = ''
-    # テンプレートに渡されるオブジェクト名を任意のものに指定できる
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # 店舗データの取得
+        restaurants = Restaurant.objects.all()
+
+        # ページネーション
+        paginator = Paginator(restaurants, self.paginate_by)
+        page_number = self.request.GET.get('page') # ページ番号の取得
+        page_obj = paginator.get_page(page_number)
+
+        context['restaurants'] = page_obj
+        context['hit_restaurants_number'] = paginator.count # 検索された店舗数
+
+        # 並び替えオプション
+        context['sort_options'] = [
+            ('掲載日が新しい順', '新しい順'),
+            ('価格が安い順', '価格が安い順'),
+            ('価格が高い順', '価格が高い順'),
+            ('予約数順', '予約数順'),
+        ]
+
+        return context
+
+        
