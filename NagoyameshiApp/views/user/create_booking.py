bfd735b7c3d98797ff2223b63356b1b6c9b708fe
ayaconsa <aya.consadole@gmail.com>
2024-06-18 21:49:53 +0900

    定休日を選択できないように設定。flatpicker。

--- NagoyameshiApp/views/user/create_booking.py
+++ NagoyameshiApp/views/user/create_booking.py
@@ -4,6 +4,7 @@ from NagoyameshiApp.models.restaurant import Restaurant
 from NagoyameshiApp.forms import BookingForm
 from django.urls import reverse
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +16,14 @@ class CreateBookingView(TemplateView):
         context = super().get_context_data(**kwargs)
         restaurant_id = self.kwargs.get('pk')
         restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+
+        # close_dayの内容を確認	
+        close_days = restaurant.close_day.split(', ')	
+        logger.debug(f'close_days: {close_days}')
+
         context['restaurant'] = restaurant
+
+        context['close_day'] = json.dumps([self.day_to_number(day) for day in close_days])
 
         if self.request.user.is_authenticated and self.request.user.subscription:
             context['booking_form'] = BookingForm()
@@ -25,6 +33,7 @@ class CreateBookingView(TemplateView):
     def post(self, request, *args, **kwargs):
         restaurant_id = self.kwargs.get('pk')
         restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+
 
         if request.user.is_authenticated and request.user.subscription:
             form = BookingForm(request.POST)
@@ -40,3 +49,15 @@ class CreateBookingView(TemplateView):
 
         logger.warning("Booking failed, reloading the page")
         return self.get(request, *args, **kwargs)
+
+    def day_to_number(self, day):
+        days = {
+            '月曜日': 1,
+            '火曜日': 2,
+            '水曜日': 3,
+            '木曜日': 4,
+            '金曜日': 5,
+            '土曜日': 6,
+            '日曜日': 0
+        }
+        return days.get(day, -1)
