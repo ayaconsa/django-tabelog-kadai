e3efe699856eb17edb5e5e49104cb98088336a15
ayaconsa <aya.consadole@gmail.com>
2024-06-19 21:36:50 +0900

    ページタイトルの統一

--- NagoyameshiApp/views/user/change_payment_method.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-# お支払い方法変更（未実装）
-class ChangePaymentMethodView(TemplateView):
-    template_name = "NagoyameshiApp/user/change_payment_method.html"
--- NagoyameshiApp/views/user/delete_account.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-# アカウント削除（会員のみ）
-class DeleteAccountView(TemplateView):
-    template_name = "NagoyameshiApp/user/delete_account.html"
