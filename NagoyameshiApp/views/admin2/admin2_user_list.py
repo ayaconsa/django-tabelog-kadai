c9072fda8db09b4d99b893ed94ba4c71547de0fc
ayaconsa <aya.consadole@gmail.com>
2024-06-22 15:23:25 +0900

    会員一覧の偶数業の背景色変更、幅調整

--- NagoyameshiApp/views/admin2/admin2_user_list.py
+++ NagoyameshiApp/views/admin2/admin2_user_list.py
@@ -1,6 +1,6 @@
+from typing import Any
 from django.views.generic import ListView
 from NagoyameshiApp.models.custom_user import CustomUser
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.shortcuts import redirect
 
 # 管理者：会員一覧
@@ -15,3 +15,23 @@ class UserListView(ListView):
         if not self.request.user.is_authenticated:
             return redirect('login')  # ログインしていない場合、ログインページにリダイレクト
         return redirect('top')  # スタッフでない場合、トップページにリダイレクト
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sort_option = self.request.GET.get('sort', 'id') # デフォルトは会員ID順
+        if sort_option == 'name':
+            context['object_list'] = CustomUser.objects.all().order_by('furigana')
+        elif sort_option == 'birthday':
+            context['object_list'] = CustomUser.objects.all().order_by('birthday')
+        elif sort_option == 'subscription':
+            context['object_list'] = CustomUser.objects.all().order_by('-subscription', 'id')
+        else:
+            context['object_list'] = CustomUser.objects.all().order_by('id')
+        context['sort_options'] = [
+            ('id', '会員番号順'),
+            ('name', 'フリガナ順'),
+            ('birthday', '生年月日順'),
+            ('subscription', 'サブスク会員')
+        ]
+        context['selected_sort'] = sort_option
+        return context
